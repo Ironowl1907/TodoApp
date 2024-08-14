f10e2c239700461022d1dc384bbbc252f03de471
Ironowl <facuguialtana7@gmail.com>
2024-08-14 10:30:47 -0300

    Set priority added

--- main.go
+++ main.go
@@ -149,7 +149,7 @@ func main() {
 
 				if line[0] != args[0] {
 					filteredDBBuffer = append(filteredDBBuffer, line)
-				}	else {
+				} else {
 					deleted = true
 				}
 			}
@@ -282,7 +282,7 @@ func main() {
 	var CMDEdit = &cobra.Command{
 		Use:   "edit [task ID] [new taks name]",
 		Short: "Edits a task",
-		Args:  cobra.MinimumNArgs(1),
+		Args:  cobra.MinimumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			var edited bool = false
 			DB, err := checkOrCreateDB(DefaultDBName)
@@ -308,7 +308,7 @@ func main() {
 				if line[0] != args[0] {
 					filteredDBBuffer = append(filteredDBBuffer, line)
 				} else {
-					line = []string{line[0], strings.Join(args[1:], " "), line[2]}
+					line = []string{line[0], strings.Join(args[1:], " "), line[2], line[3]}
 					filteredDBBuffer = append(filteredDBBuffer, line)
 					edited = true
 				}
@@ -335,6 +335,67 @@ func main() {
 			}
 
 			csvWriter.Flush()
+			fmt.Printf("Task with ID %s updated\n", args[0])
+		},
+	}
+
+	var CMDPrior = &cobra.Command{
+		Use:   "prioritize [task ID] [new taks priority]",
+		Short: "Edits a task priority",
+		Args:  cobra.MinimumNArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			var edited bool = false
+			DB, err := checkOrCreateDB(DefaultDBName)
+			if err != nil {
+				panic(err)
+			}
+			defer DB.Close()
+
+			var filteredDBBuffer [][]string
+			reader := csv.NewReader(DB)
+			records, err := reader.ReadAll()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			for _, line := range records {
+				if len(line) < 3 {
+					fmt.Println("[Error] Invalid record format:", line)
+					continue
+				}
+
+				if line[0] != args[0] {
+					filteredDBBuffer = append(filteredDBBuffer, line)
+				} else {
+					line = []string{line[0], line[1], args[1], line[3]}
+					filteredDBBuffer = append(filteredDBBuffer, line)
+					edited = true
+				}
+			}
+
+			if !edited {
+				println("No matching ID in the database")
+				return
+			}
+
+			// Recreate the DB with the edited one
+			DB, err = os.Create(DefaultDBName)
+			if err != nil {
+				fmt.Println("[Error] Error while writing to new file:", err)
+				return
+			}
+			defer DB.Close()
+
+			csvWriter := csv.NewWriter(DB)
+			err = csvWriter.WriteAll(filteredDBBuffer)
+			if err != nil {
+				fmt.Println("[Error] Couldn't write data:", err)
+				return
+			}
+
+			csvWriter.Flush()
+			fmt.Printf("Task with ID %s prioritized to level %s\n", args[0], args[1])
 		},
 	}
 
@@ -349,5 +410,6 @@ func main() {
 	rootCmd.AddCommand(CMDEdit)
 	CMDList.Flags().BoolP("undone", "u", false, "Lists all the undone (non deleted) tasks")
 	rootCmd.AddCommand(CMDDone)
+	rootCmd.AddCommand(CMDPrior)
 	rootCmd.Execute()
 }
